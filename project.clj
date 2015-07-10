94d0561dab5dd83ff66e88eef597434fe899cf9c
Yoko Harada <yokolet@gmail.com>
2015-07-10 00:18:18 -0400

    Updates welcome app with background image

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Welcome to ClojureBridge InstallFest app"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [quil "2.2.4"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [quil "2.2.6"]])
--- src/welcometoclojurebridge/core.clj
+++ src/welcometoclojurebridge/core.clj
@@ -3,14 +3,15 @@
             [quil.middleware :as m]))
 
 (def angle-speed 0.05)
-(def color-speed 0.07)
+(def color-speed 0.15)
 
 (defn setup []
   (q/frame-rate 30)
   (q/color-mode :hsb)
-  (q/text-font (q/create-font "Helvetica" 32))
+  (q/text-font (q/create-font "resources/SourceCodePro-Regular.ttf" 36 true))
 
   {:logo (q/load-image "logo.png")
+   :background (q/load-image "background.png")
    :color 0
    :angle 0})
 
@@ -20,25 +21,24 @@
       (update-in [:angle] + angle-speed)))
 
 (defn draw-state [state]
-  (q/background 255)
+  (q/background-image (:background state))
 
   (let [angle (:angle state)
         x (* 150 (q/cos angle))
         y (* 150 (q/sin angle))]
-    (q/fill (:color state) 255 255)
-    (q/text "Welcome to"     140 (min 180 (+ 190 y)))
+    (q/fill (mod (:color state) 80) (mod (:color state) 180) (mod (:color state) 150))
+    (q/text "Welcome to"     190 (min 180 (+ 190 y)))
     (q/text "You are ready to code!" 80 (max 330 (+ 330 y)))
 
     (q/with-translation [(/ (q/width) 2)
                          (/ (q/height) 2)]
-      (q/image (:logo state) (- x 50) (- y 50) 100 100))))
+      (q/image (:logo state) (- x 50) (- y 50) 110 110))))
 
 (q/defsketch welcometoclojurebridge
   :title "Welcome To ClojureBridge!"
-  :size [500 500]
+  :size [650 500]
   :setup setup
   :update update-state
   :draw draw-state
+  :features [:keep-on-top]
   :middleware [m/fun-mode])
-
-
