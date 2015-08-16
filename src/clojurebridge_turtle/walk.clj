99eec30c38ca9420167f8a7fe5600a69d85c8467
Yoko Harada <yokolet@gmail.com>
2015-08-15 23:08:20 -0400

    Fixes comments

--- src/clojurebridge_turtle/walk.clj
+++ src/clojurebridge_turtle/walk.clj
@@ -5,15 +5,20 @@
 (home)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; evaluate whole file by hitting
+;;   ctrl + shift + enter, or
+;;   cmd + shift + enter
+;;
 ;; add some functions under these comment lines
+;; evaluate each line by hitting
+;;   ctrl + enter,  or
+;;   cmd + enter
 ;;
 ;; for example
 ;; (forward 30)
 ;; (right 90)
 ;; (forward 30)
 ;; (right 90)
-;;
-;; evaluate code by hitting cmd/ctrl + shift + enter
 ;;
 ;; see how turtle walks
 ;;
